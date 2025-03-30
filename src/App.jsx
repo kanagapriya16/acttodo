71eaacd90607ab5c9b6c2482e25af51bc1b3644c
kanagapriya16 <135326850+kanagapriya16@users.noreply.github.com>
2025-03-30 12:46:57 +0530

    eigth commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Actdomain from './Actdomain'
 
-
+const basename = import.meta.env.MODE === "production" ? "/acttodo" : "/";
 
 function App() {
 
 
   return (
     <>
-     <BrowserRouter>
+     <BrowserRouter  basename={basename}>
      <Routes>
       <Route path="/" element={<Actdomain/>}/>
      </Routes>
